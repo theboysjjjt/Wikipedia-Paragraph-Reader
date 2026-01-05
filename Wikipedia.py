2e276181759e532fdbe9b0112439e623665bb1b3
theboysjjjt <jacob.thomson2014@outlook.com>
2026-01-05 16:22:39 +0000

    Simplified link entering

--- Wikipedia.py
+++ Wikipedia.py
@@ -1,17 +1,20 @@
 
 import requests as rq
 from bs4 import BeautifulSoup as bs
-pages = int(input('how many wikipedia pages do you want to search') )
+pages = int(input('how many wikipedia pages do you want to search: ') )
 contact = input("what is your email (This is used only for wikipedias robot policy) ")
 headers = {
 'User-Agent': f'TestBot/1.0 (contact: {contact})'
 }
 for page in range(pages):
-    page = input("Web Page: ")
-    item = int(input('which paragraph of the article do you want (if you want them all answer -1)'))
-    response = rq.get(page, headers=headers)
+    page = input("What article do you want: ")
+    page.capitalize()
+    pages = f"https://en.wikipedia.org/wiki/{page}"
+    item = int(input('which paragraph of the article do you want (if you want them all answer -1) '))
+    response = rq.get(pages, headers=headers)
     soup = bs(response.text, 'html.parser')
     paragraphs = soup.find_all('p')
+    print()
     if item != -1:
         print(paragraphs[item].get_text())
     else:
